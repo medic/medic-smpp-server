31934928469c8b84df612d4653cfcce9d40968e4
Alex Anderson <anderson@medicmobile.org>
2017-03-16 12:37:07 +0545

    Handle all events

--- src/test.js
+++ src/test.js
@@ -1,5 +1,6 @@
-var smpp = require('smpp'),
-    conf = require('../config.json');
+var conf = require('../config.json'),
+    smpp = require('smpp'),
+    str = JSON.stringify;
 
 function log() {
   var args = Array.prototype.slice.call(arguments);
@@ -18,16 +19,47 @@ session.bind_transceiver({
     log('Bound.  PDU: ' + JSON.stringify(pdu));
     if (pdu.command_status == 0) {
 	log('Bind successful.');
-	log('Sending to Bishwas...');
-        session.submit_sm({
-            destination_addr: conf.test.destination,
-            short_message: 'Hello from node at ' + new Date(),
-        }, function(pdu) {
-            if (pdu.command_status == 0) {
-                log('Send succesful.  Message ID: ' + pdu.message_id);
-            }
-        });
+
+	if(conf.test.send_message) {
+	  log('Sending to ' + conf.test.destination + '...');
+	  session.submit_sm({
+	      destination_addr: conf.test.destination,
+	      short_message: 'Hello from node at ' + new Date(),
+	  }, function(pdu) {
+	      if (pdu.command_status == 0) {
+		  log('Send succesful.  Message ID: ' + pdu.message_id);
+	      }
+	  });
+	}
     }
 });
 
-log('Finished.');
+session.on('connect', function() {
+  log('EVENT:connect', 'Connection successfully established.');
+});
+
+session.on('secureConnect', function() {
+  log('EVENT:secureConnect', 'Handshaking process for a secure connection has successfully completed.');
+});
+
+session.on('close', function() {
+  log('EVENT:close', 'Connection has been fully closed.');
+});
+
+session.on('error', function(error) {
+  log('EVENT:error', 'Event occurred.  Close will be called after event handling finishes.', error);
+});
+
+session.on('send', function(pdu) {
+  log('EVENT:send', 'PDU sent.', str(pdu));
+});
+
+session.on('pdu', function(pdu) {
+  log('EVENT:pdu', 'PDU received.', str(pdu));
+});
+
+session.on('unknown', function(pdu) {
+  log('EVENT:unknown', 'Unknown PDU received.', str(pdu));
+});
+
+log('If this is the last message, bind probably failed.');
